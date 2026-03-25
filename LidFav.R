ba5c493e230f990755ccd7866e1c825f5eb84d1e
tiago-regueira <tiago.regueira.arq@gmail.com>
2026-03-25 14:07:26 -0300

    Add files via upload

--- LidFav.R
+++ LidFav.R
@@ -9,3 +9,14 @@
 setwd( this.path::this.dir() )
 
 source( "./LidFav_func.R" )
+
+assentamentos_pesquisa <- c( 'JARDIM NOVA PANTANAL',
+                             'JARDIM NAKAMURA I',
+                             'VILA RUBI',
+                             'JARDIM IMBUIAS I',
+                             'CHÁCARA DO CONDE II',
+                             'JARDIM ARACATI I' )
+
+indicadores <- calcula_indicadores_favelas_nucleos( assentamentos_pesquisa )
+
+indicadores
--- LidFav_func.R
+++ LidFav_func.R
@@ -15,7 +15,7 @@ library( sf )
 
 ## 1 Insumos
 
-calcula_indicadores_favelas <- function(nomes_favelas) {
+calcula_indicadores_favelas_nucleos <- function( nomes ) {
   
   library(sf)
   library(dplyr)
@@ -23,28 +23,61 @@ calcula_indicadores_favelas <- function(nomes_favelas) {
   library(lidR)
   
   #### Dados base
-  favelas <- st_read("./1_Insumos/Favelas_Habita_20260307.gpkg")
+  favelas <- st_read("./1_Insumos/Favelas_Habita_20260307.gpkg") |>
+    mutate(tipo = "favela")
+  
+  nucleos <- st_read("./1_Insumos/Nucleos_Habita_20260325.gpkg") |>
+    mutate(tipo = "nucleo")
+  
+  assentamentos <- bind_rows(favelas, nucleos)
+  
+  nomes_validos <- assentamentos$nome
+  
+  nomes_invalidos <- setdiff(nomes, nomes_validos)
+  
+  if (length(nomes_invalidos) > 0) {
+    stop(
+      paste0(
+        "Os seguintes nomes não foram encontrados nas bases de favelas/núcleos:\n",
+        paste(nomes_invalidos, collapse = ", ")
+      )
+    )
+  }
   
   malha_mdt_mds <- st_read("./1_Insumos/Quadricula_MDT_MDS_2020.geojson") |>
     st_set_crs('EPSG:31983')
   
   pasta_base <- "./2_Processamento"
   
-  processa_uma_favela <- function(nome_favela) {
+  processa_um <- function(nome_area) {
     
-    message("Processando: ", nome_favela)
+    message("Processando: ", nome_area)
     
-    favela_selecionada <- favelas |>
-      filter(nome == nome_favela)
+    area_sel <- assentamentos |>
+      filter(nome == nome_area)
     
-    favela_buffer <- st_buffer(favela_selecionada, 120)
+    if (nrow(area_sel) == 0) {
+      warning("Nome não encontrado: ", nome_area)
+      return(NULL)
+    }
     
-    pasta_favela <- file.path(pasta_base, nome_favela)
+    tipo_area <- area_sel$tipo[1]
     
-    dir.create(pasta_favela, recursive = TRUE, showWarnings = FALSE)
-    dir.create(file.path(pasta_favela, "MDT"), showWarnings = FALSE)
-    dir.create(file.path(pasta_favela, "MDS"), showWarnings = FALSE)
-    dir.create(file.path(pasta_favela, "Resultados"), showWarnings = FALSE)
+    pasta_area <- file.path(pasta_base, nome_area)
+    pasta_result <- file.path(pasta_area, "Resultados")
+    rds_path <- file.path(pasta_result, "indicadores.rds")
+    
+    #### CACHE: se já existe RDS, retorna direto
+    if (file.exists(rds_path)) {
+      return(readRDS(rds_path))
+    }
+    
+    favela_buffer <- st_buffer(area_sel, 120)
+    
+    dir.create(pasta_area, recursive = TRUE, showWarnings = FALSE)
+    dir.create(file.path(pasta_area, "MDT"), showWarnings = FALSE)
+    dir.create(file.path(pasta_area, "MDS"), showWarnings = FALSE)
+    dir.create(pasta_result, showWarnings = FALSE)
     
     #### Seleção de quadrículas
     malha_selecao <- st_intersection(malha_mdt_mds, favela_buffer)
@@ -53,35 +86,38 @@ calcula_indicadores_favelas <- function(nomes_favelas) {
       st_drop_geometry() |>
       pull(cd_quadricula)
     
-    extensoes <- c('MDS', 'MDT')
+    extensoes <- c("MDS", "MDT")
     
-    #### Download
+    #### Download (com verificação)
     for (extensao in extensoes) {
       
-      pasta_destino <- file.path(pasta_favela, extensao)
+      pasta_destino <- file.path(pasta_area, extensao)
       
       for (codigo in malha_download) {
         
-        url <- paste0(
-          "http://download.geosampa.prefeitura.sp.gov.br/PaginasPublicas/",
-          "downloadArquivo.aspx?orig=DownloadMapaArticulacao",
-          "&arq=", extensao, "_2020%5C", codigo, ".zip",
-          "&arqTipo=MAPA_ARTICULACAO"
-        )
-        
         zip_destino <- file.path(pasta_destino, paste0(codigo, ".zip"))
         
-        download.file(url, zip_destino, mode = "wb")
-        unzip(zip_destino, exdir = pasta_destino)
-        
-        Sys.sleep(1)
+        if (!file.exists(zip_destino)) {
+          
+          url <- paste0(
+            "http://download.geosampa.prefeitura.sp.gov.br/PaginasPublicas/",
+            "downloadArquivo.aspx?orig=DownloadMapaArticulacao",
+            "&arq=", extensao, "_2020%5C", codigo, ".zip",
+            "&arqTipo=MAPA_ARTICULACAO"
+          )
+          
+          download.file(url, zip_destino, mode = "wb")
+          unzip(zip_destino, exdir = pasta_destino)
+          
+          Sys.sleep(1)
+        }
       }
     }
     
     #### Processamento
     for (extensao in extensoes) {
       
-      pasta_destino <- file.path(pasta_favela, extensao)
+      pasta_destino <- file.path(pasta_area, extensao)
       
       ctg <- readLAScatalog(pasta_destino) |>
         clip_roi(favela_buffer)
@@ -95,7 +131,7 @@ calcula_indicadores_favelas <- function(nomes_favelas) {
         r <- rasterize_terrain(ctg, res = 1, tin())
       }
       
-      r <- mask(r, vect(favela_selecionada))
+      r <- mask(r, vect(area_sel))
       
       writeRaster(
         r,
@@ -104,9 +140,10 @@ calcula_indicadores_favelas <- function(nomes_favelas) {
       )
     }
     
-    #### Remover LAS/LAZ
+    #### Limpeza LAS/LAZ
     for (extensao in extensoes) {
-      pasta_destino <- file.path(pasta_favela, extensao)
+      
+      pasta_destino <- file.path(pasta_area, extensao)
       
       arquivos_las <- list.files(
         pasta_destino,
@@ -118,48 +155,54 @@ calcula_indicadores_favelas <- function(nomes_favelas) {
     }
     
     #### CHM
-    mds <- rast(file.path(pasta_favela, "MDS", "MDS_mosaico.tif"))
-    mdt <- rast(file.path(pasta_favela, "MDT", "MDT_mosaico.tif"))
+    mds <- rast(file.path(pasta_area, "MDS", "MDS_mosaico.tif"))
+    mdt <- rast(file.path(pasta_area, "MDT", "MDT_mosaico.tif"))
     
-    mds_favela <- crop(mds, favela_selecionada)
-    mdt_favela <- crop(mdt, favela_selecionada)
+    mds_area <- crop(mds, area_sel)
+    mdt_area <- crop(mdt, area_sel)
     
-    chm <- mds_favela - mdt_favela
+    chm <- mds_area - mdt_area
     
     writeRaster(
       chm,
-      file.path(pasta_favela, "Resultados", "CHM.tif"),
+      file.path(pasta_result, "CHM.tif"),
       overwrite = TRUE
     )
     
     #### Indicadores
     edificacoes <- chm > 2
     
-    area_favela <- as.numeric(st_area(favela_selecionada))
+    area_total <- as.numeric(st_area(area_sel))
     area_pixel <- prod(res(chm))
     
-    n_pixels_edif <- global(edificacoes, "sum", na.rm = TRUE)[1]
-    area_edificada <- n_pixels_edif * area_pixel
+    n_pixels <- global(edificacoes, "sum", na.rm = TRUE)[1]
+    area_construida <- n_pixels * area_pixel
     
-    TI <- area_edificada / area_favela
+    TO <- area_construida / area_total
     
     altura_edif <- mask(chm, edificacoes, maskvalues = FALSE)
     volume_pixel <- altura_edif * area_pixel
     volume_total <- global(volume_pixel, "sum", na.rm = TRUE)[1,1]
     
-    IV <- volume_total / area_favela
+    IV <- volume_total / area_total
     
-    data.frame(
-      favela = nome_favela,
-      area_edificada = area_edificada,
-      TI = TI,
+    resultado <- data.frame(
+      nome = nome_area,
+      tipo = tipo_area,
+      area_construida = area_construida,
+      TO = TO,
       volume_total = volume_total,
       IV = IV
     )
+    
+    #### Salva cache
+    saveRDS(resultado, rds_path)
+    
+    return(resultado)
   }
   
   #### Iteração
-  resultados <- lapply(nomes_favelas, processa_uma_favela) |>
+  resultados <- lapply(nomes, processa_um) |>
     bind_rows()
   
   return(resultados)
